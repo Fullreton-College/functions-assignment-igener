f0cf6e542473cf6c30a18f74d858c098d1b45748
igener <icegen112@gmail.com>
2026-03-16 19:06:00 -0700

    Code changes complete

--- Function_1.cpp
+++ Function_1.cpp
@@ -3,13 +3,28 @@
 using namespace std;
 
 /* Define your function here */ 
+string CoinFlip();
 
 int main() {
    // Add more variables as needed
-   
+   int cfnum;
    srand(2);  // Unique seed
 
    /* Type your code here */
-
+    cout << "Enter number of coinflips: ";
+    cin >> cfnum;
+    for(int i = 0; i < cfnum; i++){
+        cout << CoinFlip();
+    }
+    
    return 0;
+}
+
+string CoinFlip(){
+    if (rand()%2 == 1){
+        return "Heads ";
+    }
+    else{
+        return "Tails ";
+    }
 }
