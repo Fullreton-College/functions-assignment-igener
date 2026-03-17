4cca4510622cf128075b848b78f538a07af9b1d0
igener <icegen112@gmail.com>
2026-03-16 19:06:51 -0700

    Code changes complete

--- Function_2.cpp
+++ Function_2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 // We will use cents for all monetary values. This will let us
 // work with integer, rather than floating-point, variables.
-const int TWINKIE_PRICE = 350;
+const int price = 350;
 
 // Prompt the user to insert coins until enough has been paid to buy
 // a twinkie. The total amount inserted, in cents, is returned.
@@ -38,11 +38,34 @@ int main()
 
 int accept_money()
 {
-  //write your code
+  //write your code d,q, d,n
+  int money = 0;
+  string hold;
+  cout << "It costs $3.50 (350 cents) for a deep-fried twinkie." << endl;
+  while (money < price){
+      cout << "Enter coin input: ";
+      cin >> hold; 
+      if (hold == "dollar"){
+          money = money + 100;
+      }
+      else if (hold == "quarter"){
+          money = money + 25;
+      }
+      else if (hold == "dime"){
+          money = money + 10;
+      }
+      else if (hold == "nickel"){
+          money = money + 5;
+      }
+      cout << "Current Total (in cents): " << money << " cents" << endl;
+  }
+  return money;
 }
 
 int compute_change(int total_paid) 
 {
 //write your code
+return total_paid - price;
 }
+
 
